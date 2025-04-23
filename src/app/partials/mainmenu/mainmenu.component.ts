8caf49b2f22665f2bf5f8e8926d72c92894f7af2
Annika Gadman <annika.gadman@gmail.com>
2025-04-23 10:51:06 -0500

    skapat skalet av single page och routerActiveLinks

---
+++ src/app/partials/mainmenu/mainmenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-mainmenu',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './mainmenu.component.html',
+  styleUrl: './mainmenu.component.css'
+})
+export class MainmenuComponent {
+
+}
