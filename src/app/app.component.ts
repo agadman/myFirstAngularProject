ac8e8b90c3381c3526dafdb474d279cacd34cc3a
Annika Gadman <annika.gadman@gmail.com>
2025-04-23 15:23:56 -0500

    skapat footer component och lagt till grundstyling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainmenuComponent } from './partials/mainmenu/mainmenu.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainmenuComponent],
+  imports: [RouterOutlet, MainmenuComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
